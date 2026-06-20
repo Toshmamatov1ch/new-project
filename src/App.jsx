07ff909235b6093b8f8efafe8154eceadaf0bd3f
diyorbek <dtoshmamamatov77@gmail.com>
2026-06-20 16:52:05 +0500

    update struktura

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PublicLayout from "./layouts/PublicLayout";
+
 function App() {
-  return <div></div>;
+  const routers = createBrowserRouter([
+    {
+      index: true,
+      element: <PublicLayout />,
+    },
+  ]);
+  return <RouterProvider router={routers} />;
 }
 
 export default App;
--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { Outlet } from "react-router-dom";
+
+// coponents
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 function PublicLayout() {
-  return <div></div>;
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default PublicLayout;
