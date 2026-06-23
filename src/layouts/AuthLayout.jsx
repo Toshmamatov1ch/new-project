16a9300d6a531e08d15af451f67f055fc48b2e72
diyorbek <dtoshmamamatov77@gmail.com>
2026-06-23 17:19:58 +0500

    Login

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Login from "../components/Login";
+
 function AuthLayout() {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default AuthLayout;
